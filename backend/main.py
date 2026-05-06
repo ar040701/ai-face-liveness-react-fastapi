a08c26d06ce517e3c2fc328a271ba5b7b9b51abc
Ayush <raj07ayush@gmail.com>
2026-05-07 00:45:22 +0530

    Add Vercel frontend URL to CORS

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI(title="AI Face Liveness Detection API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
-        "https://YOUR-FRONTEND-URL.vercel.app"], 
+        "https://ai-face-liveness-react-fastapi.vercel.app/"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
